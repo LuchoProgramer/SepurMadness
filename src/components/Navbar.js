3cb22bc0783a09832982547da54eb112f393abbb
LuchoProgramer <luchoviteri1990@gmail.com>
2025-01-15 21:29:56 -0500

    modo oscuros iconos blancos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,10 +17,14 @@ const Navbar = () => {
                 {/* Sección de redes sociales */}
                 <div className="flex space-x-4 sm:space-x-5">
                     <a href="https://www.instagram.com/sepurmadness/profilecard/?igsh=anQ2Zm5uMnp3cmF2" target="_blank" rel="noopener noreferrer">
-                        <FaInstagram className="w-4 h-4 sm:w-4 sm:h-4 md:w-5 md:h-5 lg:w-6 lg:h-6 xl:w-6 xl:h-6 4k:w-16 4k:h-16 text-white hover:text-naranja transition duration-300 dark:text-black dark:hover:text-naranja bg-black bg-opacity-50 rounded-full p-1" />
+                        <FaInstagram className="w-4 h-4 sm:w-4 sm:h-4 md:w-5 md:h-5 lg:w-6 lg:h-6 xl:w-6 xl:h-6 4k:w-16 4k:h-16 
+            text-white hover:text-naranja transition duration-300 
+            dark:text-white dark:hover:text-naranja bg-black bg-opacity-50 rounded-full p-1" />
                     </a>
                     <a href="https://www.youtube.com/@sepurmadness9489" target="_blank" rel="noopener noreferrer">
-                        <FaYoutube className="w-4 h-4 sm:w-4 sm:h-4 md:w-5 md:h-5 lg:w-6 lg:h-6 xl:w-6 xl:h-6 4k:w-16 4k:h-16 text-white hover:text-naranja transition duration-300 dark:text-black dark:hover:text-naranja bg-black bg-opacity-50 rounded-full p-1" />
+                        <FaYoutube className="w-4 h-4 sm:w-4 sm:h-4 md:w-5 md:h-5 lg:w-6 lg:h-6 xl:w-6 xl:h-6 4k:w-16 4k:h-16 
+            text-white hover:text-naranja transition duration-300 
+            dark:text-white dark:hover:text-naranja bg-black bg-opacity-50 rounded-full p-1" />
                     </a>
                 </div>
 
